75128a757cc06f1427982d682c252f3244696f0d
Alexis Louis <wimacod@gmail.com>
2020-11-19 18:47:01 +0100

    Adds notebook class

--- server/Fiesta.py
+++ server/Fiesta.py
@@ -6,10 +6,10 @@ class Fiesta():
 
     Attributes
     ----------
-    connected_user
-        Number of simultaneous connected users
-    n_players
-        Number of players
+    connections
+        List of session ids
+    players
+        List of player dictionnaries
 
     """
     def __init__(self):
@@ -52,5 +52,44 @@ class Fiesta():
         self.connections.remove(sid)
 
 class Notebook():
+    """Notebook of Fiesta game.
+
+    The Notebook class emulates a physical notebook belonging to a player.
+
+    Attributes
+    ----------
+    player
+        owner of the notebook
+    drawing
+        list of rounds drawings
+    drawing
+        list of rounds words
+    round
+        current round of the notebook
+
+    """
     def __init__(self, owner):
-        self.owner = owner
+        self.player = player
+        self.drawings = []
+        self.words = []
+        self.round = 0
+    
+    def add_word(self, word):
+        """ Adds a word to the notebook and increments round.
+        Attributes
+        ----------
+        word
+            string of written word
+        """
+        self.words.append({self.round : word})
+        self.round += 1
+
+    def add_drawing(self, drawing):
+        """ Adds a drawing to the notebook and increments round.
+        Attributes
+        ----------
+        drawing
+            drawing data
+        """
+        self.words.append({self.round : drawing})
+        self.round += 1
