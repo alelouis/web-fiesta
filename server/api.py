f479d3487af556ab40e1c7614426a73615cf6762
Alexis Louis <wimacod@gmail.com>
2020-11-19 11:30:43 +0100

    - Adds index client template - Adds websockets handling to serve - Adds flask-socketio to conda .yml

--- server/api.py
+++ server/api.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, make_response
 from flask_restful import Resource, Api
+from flask_socketio import SocketIO
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder= '../client/templates/')
 api = Api(app)
+socketio = SocketIO(app)
 
-class HelloWorld(Resource):
+# Sockets
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
+# REST
+class Welcome(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return make_response(render_template('index.html'))
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(Welcome, '/')
 
+
+# Launch application
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
+    #app.run(debug=True)
