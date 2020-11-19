4fd7a176d4efb82f8e75fe3b4c5e39904e554bd5
Alexis Louis <wimacod@gmail.com>
2020-11-19 18:48:21 +0100

    Separate classes for Notebook and Fiesta

--- server/Fiesta.py
+++ server/Fiesta.py
@@ -1,3 +1,5 @@
+import Notebook
+
 class Fiesta():
     """Fiesta game state class.
 
@@ -50,46 +52,3 @@ class Fiesta():
             session id of the connection
         """
         self.connections.remove(sid)
-
-class Notebook():
-    """Notebook of Fiesta game.
-
-    The Notebook class emulates a physical notebook belonging to a player.
-
-    Attributes
-    ----------
-    player
-        owner of the notebook
-    drawing
-        list of rounds drawings
-    drawing
-        list of rounds words
-    round
-        current round of the notebook
-
-    """
-    def __init__(self, owner):
-        self.player = player
-        self.drawings = []
-        self.words = []
-        self.round = 0
-    
-    def add_word(self, word):
-        """ Adds a word to the notebook and increments round.
-        Attributes
-        ----------
-        word
-            string of written word
-        """
-        self.words.append({self.round : word})
-        self.round += 1
-
-    def add_drawing(self, drawing):
-        """ Adds a drawing to the notebook and increments round.
-        Attributes
-        ----------
-        drawing
-            drawing data
-        """
-        self.words.append({self.round : drawing})
-        self.round += 1
